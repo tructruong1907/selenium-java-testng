87ed4b7a6dc6120b1b22e74f4f796dcd0e5d5422
Truc Truong <cecilia.ttruc@gmail.com>
2026-03-19 14:40:19 +0700

    create Topic_00_template create Topic_06_locator ignore folder .idea

---
+++ selenium/webdriver/Topic_06_Locator.java
@@ -0,0 +1,39 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class Topic_06_Locator {
+        WebDriver driver;
+
+   @BeforeClass //gắn chỉ dẫn
+        //mở web browser
+    public void InitialBrowser(){
+        driver = new ChromeDriver();
+        driver.get("https://demo.nopcommerce.com/");
+    }
+
+    //execute : viết test case
+    //cú pháp HTML: Tagname (thẻ) - Attribute (thuộc tính) - Value (giá trị thuộc tính)
+    //cú pháp XPath: //tagname[@attribute = 'value']
+    //cú pháp CSS:  tagname[attribute = 'value']
+
+    @Test //gắn chỉ dẫn
+    public void TC_01_(){
+
+    }
+    @Test
+    public void TC_02_(){
+
+    }
+    @AfterClass //gắn chỉ dẫn
+    //Đóng trình duyệt
+    public void closeBrowser(){
+        driver.quit();
+    }
+
+}
